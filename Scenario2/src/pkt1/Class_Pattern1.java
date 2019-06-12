176a00f4005fc4889734b816191d36513ee74e7a
Raviendra <rspanwar@gmail.com>
2019-06-12 23:39:34 +0530

    Adding new Repo

---
+++ Scenario2/src/pkt1/Class_Pattern1.java
@@ -0,0 +1,16 @@
+package pkt1;
+
+public class Class_Pattern1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0;i<=5;i++) {
+			for (int j =1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Scenario2/src/pkt1/Class_Pattern6.java
@@ -0,0 +1,17 @@
+package pkt1;
+
+public class Class_Pattern6 {
+
+	public static void main(String[] args) {
+			
+		for(int i = 0;i<6;i++) {
+			for (int j =5;j>i;j--) {
+				System.out.print(" ");
+			}
+			     for (int k=0; k<(2*i)-1;k++) {
+				       System.out.print("*");
+		         }
+			System.out.println();
+        }
+    }
+}

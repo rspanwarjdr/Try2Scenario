74227853e1024dbce6805fd8f40c1e5405170eb9
Raviendra <rspanwar@gmail.com>
2019-06-12 23:41:06 +0530

    udated code for pattern 6

--- Scenario2/src/pkt1/Class_Pattern6.java
+++ Scenario2/src/pkt1/Class_Pattern6.java
@@ -9,7 +9,7 @@ public class Class_Pattern6 {
 				System.out.print(" ");
 			}
 			     for (int k=0; k<(2*i)-1;k++) {
-				       System.out.print("*");
+				       System.out.print("**");
 		         }
 			System.out.println();
         }
